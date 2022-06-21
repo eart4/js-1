90a05622c8d2feb3ca70a6ee6cc4129fa194c7c8
Philipp <91840260+eart4@users.noreply.github.com>
2022-06-22 00:31:45 +0300

    Update main.js

--- js 1/js/main.js
+++ js 1/js/main.js
@@ -5,8 +5,28 @@ var nick = 'eart4' // string
 
     //сейчас используется
 
+let  let0 = 'let'
+const consta1 = 'const'
 
 
-const name = 'philipp' // string
-let age = 14 // num
+
+const age = 14 // num
+
+const name = 'Philipp' // string
+
 const isProgrammer = true // boolean
+
+const _ = 'private'
+const $ = 'some value'
+
+const withNum5 = '5'
+
+// 2 мутирование
+
+console.log('Возраст человека: ' + name + ', a возраст человека: ' + age.toString()) //возращает элеменет в консоле
+
+alert('Возраст человека: ' + name + ', a возраст человека: ' + age.toString()) //вызавает меню с текстом
+
+ const lastName = prompt ( 'Введите фамилию')
+
+alert("Ваше имя: " + name + " " + lastName)
