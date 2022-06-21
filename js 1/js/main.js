e20baea35cb17af2c7c8454d82710ca8bd275df6
Philipp <91840260+eart4@users.noreply.github.com>
2022-06-21 23:16:05 +0300

    Add files via upload

---
+++ js 1/js/main.js
@@ -0,0 +1,12 @@
+// 1 переменные
+
+    // устарело
+var nick = 'eart4' // string
+
+    //сейчас используется
+
+
+
+const name = 'philipp' // string
+let age = 14 // num
+const isProgrammer = true // boolean
